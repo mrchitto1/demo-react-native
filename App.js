4a6de965f55db44dadc476b486ad9e1c7043897f
Muhammad Usman <mrchitto1@gmail.com>
2021-10-29 01:27:11 +0500

    second commit

--- App.js
+++ App.js
@@ -1,60 +1,50 @@
-import { placeholder } from '@babel/types';
+import {placeholder} from '@babel/types';
 import React from 'react';
-import {Button, Pressable, Text, View, Separator, TouchableOpacity, Image, TextInput, StyleSheet} from 'react-native';
+import {
+  Button,
+  Pressable,
+  Text,
+  View,
+  Separator,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const App = () => {
   return (
     <View style={styles.firstV}>
-      <Image
-      style={styles.imgStyle} 
-      source={require('./fbw.png')} />
-    <TextInput
-      style={styles.textInputEmail}
-        placeholder= "Email or Phone"
-    
-    />
-    
-    <TextInput
-    secureTextEntry={true}
+      <Image style={styles.imgStyle} source={require('./fbw.png')} />
+      <TextInput style={styles.textInputEmail} placeholder="Email or Phone" />
+
+      <TextInput
+        secureTextEntry={true}
         style={styles.textInputPassword}
-          placeholder="Password"
+        placeholder="Password"
       />
 
+      <TouchableOpacity style={styles.buttonStyle}>
+        <Text style={styles.fontStyle}>LOG IN</Text>
+      </TouchableOpacity>
 
-<TouchableOpacity
-style={styles.buttonStyle}
->
-<Text style={styles.fontStyle}>LOG IN</Text>
+      <TouchableOpacity style={styles.signUpStyle}>
+        <Text style={styles.fontStyle}>Sign Up for Facebook</Text>
+      </TouchableOpacity>
 
-</TouchableOpacity>
- 
-    <TouchableOpacity style={styles.signUpStyle}>
-      <Text
-      style={styles.fontStyle}
-      >
-        Sign Up for Facebook
-      </Text>
-    </TouchableOpacity>
-
-    <View 
-    style={styles.forgotPassView}>
-
-<TouchableOpacity>
-        <Text
-        style={styles.fontStyle}
-        >
-        Forgot Password?
-        </Text>
-</TouchableOpacity> 
-<TouchableOpacity>
-<Image
-      style={styles.qmImgStyle} 
-      source={require('./qm.png')} />
-</TouchableOpacity>  
-
+      <View style={styles.forgotPassView}>
+        <TouchableOpacity>
+          <Text style={styles.fontStyle}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.qmImgStyle} source={require('./qm.png')} />
+        </TouchableOpacity>
+      </View>
     </View>
-</View>
-
   );
 };
 
@@ -62,60 +52,69 @@ export default App;
 
 const styles = StyleSheet.create({
   firstV: {
-    flex: 1, alignItems: 'center', backgroundColor: '#3B5998'
-  }, 
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#3B5998',
+  },
   imgStyle: {
-    height: 80, width: 80, marginTop: 150
+    height: windowHeight/10,
+    width: '20%',
+    marginTop: windowHeight/7,
+    resizeMode:"contain"
   },
   textInputEmail: {
-    fontSize: 20,
-    height: 40,
-    width: 310,
-    marginTop: 100,
+    fontSize: windowHeight/50,
+    height: windowHeight/11,
+    width: '85%',
+    marginTop: windowHeight/7,
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
     borderWidth: 1,
-    borderColor: '#e0e0e050'
-  }, 
+    borderColor: '#e0e0e050',
+    textAlignVertical:"bottom"
+  },
   textInputPassword: {
-    fontSize: 20,
-    height: 40,
-    width: 310,
-    marginTop: 30,
+    fontSize: windowHeight/50,
+    height: windowHeight/11,
+    width: '85%',
     borderTopWidth: 0,
     borderLeftWidth: 0,
     borderRightWidth: 0,
     borderWidth: 1,
-    borderColor: '#e0e0e050'
-  }, 
+    borderColor: '#e0e0e050',
+    textAlignVertical:"bottom"
+  },
   buttonStyle: {
-    marginTop: 20, 
-    backgroundColor:'#e0e0e050', 
-    height: 40, 
-    width: 310, 
-    alignItems:'center', 
-    justifyContent:'center'
-  }, 
+    backgroundColor: '#e0e0e050',
+    height: 40,
+    width: 310,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: windowHeight/25
+  },
   signUpStyle: {
-    height: 40, marginTop: 150
+    height: windowHeight/30,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent:'center',
+    marginTop: windowHeight/10
   },
   fontStyle: {
-  fontSize: 18
+    fontSize: windowHeight/50,
   },
   forgotPassView: {
-    height: 40,
-    width:'100%', 
-    marginTop: 1, 
-    flexDirection:'row', 
-    paddingHorizontal: 50,
-    justifyContent:'space-between',
-    alignItems:'center',
-    paddingRight: 60,
-    paddingLeft: 148
+    height: windowHeight/15,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: windowWidth/3,
+    paddingRight: windowWidth/7
   },
-  qmImgStyle:{
-    height: 30, width: 30
+  qmImgStyle: {
+    height: windowHeight/20,
+    width: windowWidth/10,
+    resizeMode: 'contain'
   },
-  
-  });
+});
